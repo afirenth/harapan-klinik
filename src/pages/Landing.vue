3c9041b6869e3fea4a3a4721f7256af061190c5e
afirenth <afirenth@gmail.com>
2024-11-09 19:36:17 +0700

    update

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,79 +1,43 @@
 <template>
   <div class="grid place-items-center min-h-screen">
-    <div class="relative w-full md:hidden">
+    <!-- Tampilan Mobile -->
+    <div class="relative w-full flex flex-col md:hidden">
       <div class="absolute top-0 w-full h-96 bg-primary rounded-b-xl z-10"></div>
       <div class="absolute top-0 w-full h-full bg-white z-0"></div>
       <div class="relative z-20 pt-6">
-        <img src="../assets/logo-big.svg" alt="" class="m-auto" />
-        <div class="m-4 mt-10 p-4 sm:p-12 bg-pure-white shadow-xl rounded-lg">
-          <p class="text-lg font-bold text-primary text-center">Selamat datang di Harapan Klinik</p>
-          <p class="text-primary text-center mt-2">Kesehatan Anda, prioritas kami. Antri lebih mudah, pelayanan
-            lebih
-            cepat.</p>
-          <hr class="text-primary mt-6 w-2/4 m-auto" />
-          <hr class="text-primary mt-2 w-3/4 m-auto" />
-          <hr class="text-primary mt-2 w-full m-auto" />
-          <p class="mt-4 text-primary sm:text-center">Silahkan <span class="font-bold">Login</span> untuk melanjutkan ke
-            Aplikasi
-          </p>
-
-          <LoginCard />
-
-          <p class="mt-4 text-primary sm:text-center">Belum punya akun? <span class="font-bold">Daftar Sekarang!</span>
-          </p>
-
-          <RegisterCard />
+        <HeroSection />
+        <div class="m-4 mt-10">
+          <AuthCard />
         </div>
-
-        <div class="m-4 p-4">
+        <div class="px-4">
           <Information />
         </div>
       </div>
     </div>
+
+    <!-- Tampilan Desktop -->
     <div class="hidden md:flex w-full h-full lg:h-fit lg:w-10/12 bg-primary lg:rounded-lg items-center">
-      <div class="flex-1 p-8"><img src="../assets/logo-big.svg" alt="" class="mb-12 mx-auto">
-        <Information />
+      <div class="flex-1 p-8">
+        <HeroSection />
       </div>
       <div class="flex-1 p-8">
-        <div class="relative z-20">
-          <div class="p-4 sm:p-12 bg-pure-white shadow-xl rounded-lg">
-            <p class="text-lg font-bold text-primary text-center">Selamat datang di Harapan Klinik</p>
-            <p class="text-primary text-center mt-2">Kesehatan Anda, prioritas kami. Antri lebih mudah, pelayanan
-              lebih
-              cepat.</p>
-            <hr class="text-primary mt-6 w-2/4 m-auto" />
-            <hr class="text-primary mt-2 w-3/4 m-auto" />
-            <hr class="text-primary mt-2 w-full m-auto" />
-            <p class="mt-4 text-primary sm:text-center">Silahkan <span class="font-bold">Login</span> untuk melanjutkan
-              ke
-              Aplikasi
-            </p>
-
-            <LoginCard />
-
-            <p class="mt-4 text-primary sm:text-center">Belum punya akun? <span class="font-bold">Daftar
-                Sekarang!</span>
-            </p>
-
-            <RegisterCard />
-          </div>
-        </div>
+        <AuthCard />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import HeroSection from '../components/HeroSection.vue';
+import AuthCard from '../components/AuthCard.vue';
 import Information from '../components/Information.vue';
-import LoginCard from '../components/LoginCard.vue';
-import RegisterCard from '../components/RegisterCard.vue';
 
 export default {
   name: 'Landing',
   components: {
-    Information,
-    LoginCard,
-    RegisterCard
-  },
+    HeroSection,
+    AuthCard,
+    Information
+  }
 };
 </script>
